dca357cb3dfd6c84bc7feda5f97774fc5bd85c05
Shkurenko Vira <shkurenko.verra@gmail.com>
2025-05-30 20:08:45 +0300

    new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './logo/logo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, LogoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
